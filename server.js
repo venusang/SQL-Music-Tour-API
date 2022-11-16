938509023cae83276c39b843069abf174a1f4361
Venus Ang <venus@users.noreply.github.com>
2022-11-15 20:59:14 -0800

    Day 4 Activity - Sequelize CLI and Migrations

---
+++ server.js
@@ -0,0 +1,24 @@
+// DEPENDENCIES
+const express = require('express')
+const app = express()
+
+// CONFIGURATION / MIDDLEWARE
+require('dotenv').config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+
+// ROOT
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the Tour API'
+    })
+})
+
+// LISTEN
+app.listen(process.env.PORT, () => {
+    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
+})
